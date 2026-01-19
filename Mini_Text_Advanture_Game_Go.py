a290dc5a4f230a49fa34492e136d4355322e88c5
Labib Abiy <labibabiy07@gmail.com>
2026-01-19 21:27:34 +0700

    Add files via upload

--- Mini_Text_Advanture_Game_Go.py
+++ Mini_Text_Advanture_Game_Go.py
@@ -1,63 +1,56 @@
-print("Welcome to Mini Text Advanture Game")
-print("Follow the instructions to play this game! Enjoy the game!")
+print("-----Welcome to Mini Text Advanture Game-----")
+print("\nFollow the instructions to play this game! \n\n(YOU ONLY HAVE 2 CHANCES TO TRY) \nEnjoy the game!")
 
-while True:
-    #Library
-    print("Now, you are in the Great Library! This library has only one door that will lead you to the next room.")
-    Guess = input("Guess where is the key? (table, book, bottle, chair) ")
-    if Guess == "table":
-        print("Yes, You guessed the correct choice!")
-        Option = input("You got the key! what do yo do? (Go north, stay in this library) ")
-        if Option == "Go north":
-            #Corridor
-            print("Now, you are in the corridor! If you want to win this game, you must find the key that will lead "
-                  "you to finish this game!")
-            Guess1 = input("Guess where is the key? (table, lamp, bottle, chair) ")
-            if Guess1 == "bottle":
-                print("Yes, you guessed the correct choice!")
-                print("congratulations, you win!")
-                break
+attempt = 0
+room = "library"
+has_key = False
 
-            elif Guess1 == "lamp":
-                print("Oh no, you guessed the wrong choice!")
-                print("Thanks for playing!")
-                break
+while attempt < 3:
+    #===LIBRARY===
+    if room == "library":
+        print("\nNow, you are in the Great Library! \nThis library has only one door that will lead you to the next room.")
 
-            elif Guess1 == "chair":
-                print("Oh no, you guessed the wrong choice!")
-                print("Thanks for playing!")
-                break
+        #Find the key
+        if not has_key:
+            Guess = input("Guess where is the key? (table, book, bottle, chair): ").lower()
 
-            elif Guess1 == "table":
-                print("Oh no, you guessed the wrong choice!")
-                print("Thanks for playing!")
-                break
+            if Guess == "table":
+                print("Yes, You found the key!")
+                has_key = True
 
             else:
-                print("Sorry, that's not a valid choice! You lose")
-                print("Thanks for playing!")
-                break
+                attempt += 1
+                print(f"Wrong choice! Attempts left: {3 - attempt}")
 
-        elif Option == "stay in this library":
-            print("Oh no, why? So you doesn't want to continue this game!")
-            print("Okay then, Thanks for playing!")
+        #Got the key
+        else:
+            Option = input("\nYou got the key! what do yo do? (go north / stay): ").lower()
+
+            if Option == "go north":
+                room = "corridor"
+
+            elif Option == "stay":
+                attempt += 1
+                print(f"You decided to stay! \nCome on! Attempts left: {3 - attempt}")
+
+            else:
+                print("Invalid option! Try again!")
+
+    #===CORRIDOR===
+    elif room == "corridor":
+        print("\nNow, you are in the corridor! If you want to win this game, you must find the key that will lead "
+              "you to finish this game!")
+        Guess1 = input("Guess where is the key? (table, lamp, bottle, chair): ").lower()
+
+        if Guess1 == "bottle":
+            print("Yes, you guessed the correct choice!")
+            print("\n\ncongratulations, you win!")
             break
-    elif Guess == "book":
-        print("Oh no, you guessed the wrong choice!")
-        print("Thanks for playing!")
-        break
 
-    elif Guess == "bottle":
-        print("Oh no, you guessed the wrong choice!")
-        print("Thanks for playing!")
-        break
+        else:
+            attempt += 1
+            print(f"Wrong choice! Attempts left: {3 - attempt}")
 
-    elif Guess == "chair":
-        print("Oh no, you guessed the wrong choice!")
-        print("Thanks for playing!")
-        break
-
-    else:
-        print("Sorry, that's not a valid choice! You lose")
-        print("Thanks for playing!")
-        break
+#===GAME OVER===
+if attempt == 3:
+    print("\nGame over!")
